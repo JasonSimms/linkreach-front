f3fe9f6615fa7a46bc1f22bae7bff717a3698849
Jason Simms <simmsthecoder@gmail.com>
2024-02-26 17:38:47 +0100

    Navbar and router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,57 @@
 import * as React from "react";
 import "./App.css";
 import { StyledEngineProvider } from "@mui/material/styles";
-// import {
-//   CampaignTable,
-//   CreateCampaignForm,
-//   LinkInputForm,
-//   CampaignCard,
-//   BasicModal,
-// } from "./components";
-// import SimpleSnackbar from "./components/SnackBar";
-import {ErrorPage} from "./components";
+import {ErrorPage, NavBar} from "./components";
 import HomeLayout from "./layouts/Home";
 import Storyboard from "./components/Storyboard";
 
 import {
   createBrowserRouter,
-  RouterProvider,
+  RouterProvider, Outlet
 } from "react-router-dom";
+
+
+const NavbarWrapper =()=>{
+  return (
+  <div>
+      <NavBar/>
+      <Outlet/>
+  </div>
+  )
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomeLayout />,
+    element: <NavbarWrapper/>,
+        children:[
+             {
+                 path: "/", // yes, again
+                 element: <HomeLayout/>
+             },
+             {
+                 path: "/storyboard",
+                 element: <Storyboard/>
+             },
+             {
+              path: "/create",
+              element: <h1>Hello CREATE!</h1>
+          },
+        ]
   },
-  {
-    path: "/storyboard",
-    element: <Storyboard />,
-  },
-  {
-    path: "/help",
-    element: <div>Help</div>,
-  },
-  {
-    path: "/error",
-    element: <ErrorPage />,
-    errorElement: <ErrorPage />,
-  }
+  // {
+  //   path: "/storyboard",
+  //   element: <Storyboard />,
+  // },
+  // {
+  //   path: "/help",
+  //   element: <DumbBar/>,
+  // },
+  // {
+  //   path: "/error",
+  //   element: <ErrorPage />,
+  //   errorElement: <ErrorPage />,
+  // }
 ]);
 
 function App() {
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import { CreateCampaignForm } from "./CreateCampaignForm";
 import { CampaignCard } from "./CampaignCard";
 import { BasicModal } from "./Modal";
 import {ErrorPage} from "./ErrorPage";
+import { NavBar } from "./NavBar";
 
-export { CampaignTable, LinkInputForm, CreateCampaignForm, CampaignCard, BasicModal, ErrorPage}
+export { CampaignTable, LinkInputForm, CreateCampaignForm, CampaignCard, BasicModal, ErrorPage, NavBar}
