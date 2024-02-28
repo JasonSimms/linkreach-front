9c8ec2539f0063a73ff90ddd2e3132808ccbdf60
Jason Simms <simmsthecoder@gmail.com>
2024-02-28 17:13:46 +0100

    added user token to back end call in storyboard firestore function required paid plan. For quicker development updating my-bitly python project

--- src/components/Storyboard.tsx
+++ src/components/Storyboard.tsx
@@ -13,7 +13,7 @@ import SimpleSnackbar from "./SnackBar";
 
 import { mockLinks } from "../context/MockData";
 import { useUIContext } from "../context/UIContext";
-
+import { getAuth } from "firebase/auth";
 //SETUP UI SETTINGS CONTEXT AND CONSUME  TODO move this to user settings
 const ToggleDarkMode = () => {
   const { dark, toggleDark } = useUIContext();
@@ -29,10 +29,53 @@ const ToggleDarkMode = () => {
   );
 };
 
+//TEST Backend functions and pass authtoken.
+const BackendFoo = () => {
+  const [message, setMessage] = React.useState("");
+
+  let token = "";
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  if (user) {
+    user
+      .getIdToken(/* forceRefresh */ true)
+      .then(function (idToken: string) {
+        token = idToken;
+      })
+      .catch(function (error: any) {
+        console.error(error);
+        setMessage("Error getting token");
+      });
+  }
+
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const target = "http://127.0.0.1:5001/link-reach/us-central1/test2";
+    fetch(target, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({ token: "test" }),
+    }).then((res) => console.log(res));
+    // .then((data) => console.log(data));
+  };
+  return (
+    <div>
+      <button onClick={handleOnClick}>Backend Foo</button>
+      <p>Message from backend?: {message}</p>
+    </div>
+  );
+};
+
 const StoryBoard: React.FC = () => {
   return (
     <>
       <h1>Link Reach</h1>
+      <h2>Backend Foo</h2>
+      <BackendFoo />
       <h2>UI CONTEXT SETUP</h2>
       <ToggleDarkMode />
       <h2>Link Input Modal</h2>
